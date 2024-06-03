97b7242505da6d4735b3c56c18864ac609021ca1
thousanda <k0k1senda@gmail.com>
2024-06-03 13:21:17 +0900

    Initial Commit

---
+++ BubbleNote/BubbleNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleNoteApp.swift
+//  BubbleNote
+//
+//  Created by 千田航己 on 2024/06/03.
+//
+
+import SwiftUI
+
+@main
+struct BubbleNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
